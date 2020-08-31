b1e69f851b7392a7768269ea7e143c60f9e914aa
nittayoshihiro <yosshi3800@gmail.com>
2020-08-31 22:32:40 +0900

    順位システムをlistを使って実装しようとしたら、問題がいくつか発生した

--- Assets/Script/Result.cs
+++ Assets/Script/Result.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro; //TextMeshProUGUI用
-
+[RequireComponent(typeof(ScoreManager))]
 public class Result : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI m_scoreText;
     [SerializeField] TextMeshProUGUI m_rankText;
+    ScoreManager m_scoreManager;
     // Start is called before the first frame update
     void Start()
     {
+        m_scoreManager = GetComponent<ScoreManager>();
         int resultHitpoint = GameManager.getHitPoint();　//GameManagerからgetHitPointを持ってくる
         Debug.Log("スコア");
         m_scoreText.text = ("Score:\n") + resultHitpoint.ToString("0"); //Score:改行をしてresultHitpointを入れる
@@ -29,7 +31,21 @@ public class Result : MonoBehaviour
         {
             m_rankText.text = "C";
         }
-
+        //順位管理
+        var ranking = new List<int>();
+        ranking.Add(m_scoreManager.scoreRanking1);
+        ranking.Add(m_scoreManager.scoreRanking2);
+        ranking.Add(m_scoreManager.scoreRanking3);
+        foreach (var item in ranking)
+        {
+            if (item < resultHitpoint)//スコアが高かったら
+            {
+                //ranking[?] = resultHitpoint;
+                break;
+            }
+        }
+        ranking.RemoveAt(4);//四番目は削除
+        
     }
 
     // Update is called once per frame
