98b93b3c2763eea7f41de654743a10b7d4f818b3
nitta <yosshi3800@gmail.com>
2020-09-17 09:22:37 +0900

    JsonHelperを入れて、JsonUtilityでリストを扱えるようにした

--- Assets/Script/Result.cs
+++ Assets/Script/Result.cs
@@ -1,17 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro; //TextMeshProUGUI用
-[RequireComponent(typeof(ScoreManager))]
+using System.IO; //StreamWriterを使うためのシステム
+
 public class Result : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI m_scoreText;
     [SerializeField] TextMeshProUGUI m_rankText;
-    [SerializeField] ScoreManager m_scoreManager;
+    /// <summary>名前入れ</summary>
+    [SerializeField] InputField inputArea;
+    public GameObject score_object = null; // Textオブジェクト
+    string ranking;
+    //プレイヤーデータ
+    [System.Serializable]
+    public class PlayerData
+    {
+        public int resultScore;
+        public string playerName;
+    }
+    //ランキングデータ
+    [System.Serializable]
+    public class RankingDate
+    {
+        List<PlayerData> ranking = new List<PlayerData>(){};
+    }
+    RankingDate m_rankingDate = new RankingDate();//ランキングデータ
+
+    [System.Serializable]
+    public class RankingData
+    {
+        [SerializeField]
+        string playerName;
+        [SerializeField]
+        int score;
+
+        public RankingData(string name, int score)
+        {
+            this.score = score;
+            this.playerName = name;
+        }
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        m_scoreManager = GetComponent<ScoreManager>();
         int resultHitpoint = GameManager.getHitPoint();　//GameManagerからgetHitPointを持ってくる
         Debug.Log("スコア");
         m_scoreText.text = ("Score:\n") + resultHitpoint.ToString("0"); //Score:改行をしてresultHitpointを入れる
@@ -31,44 +65,48 @@ public class Result : MonoBehaviour
         {
             m_rankText.text = "C";
         }
-        //順位管理
-        List<int> ranking = new List<int>();
-        ranking.Add(m_scoreManager.scoreRanking1);
-        ranking.Add(m_scoreManager.scoreRanking2);
-        ranking.Add(m_scoreManager.scoreRanking3);
-        ranking.Add(resultHitpoint);
-        //ソート
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 4; j++)
-            {
-                if((j > 0) && (ranking[j - 1] > ranking[j]))
-                {
-                    Swap( ranking[j - 1], ranking[j]);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-        ranking.RemoveAt(4);//四番目は削除
+        //テスト
+        RankingData a = new RankingData("Nitta", 150);
+        RankingData b = new RankingData("Kato",200);
+        RankingData c = new RankingData("Suzuki", 300);
+        string d = JsonUtility.ToJson(a);
+        Debug.Log(d);
 
-        m_scoreManager.scoreRanking1 += ranking[1];
-        m_scoreManager.scoreRanking2 += ranking[2];
-        m_scoreManager.scoreRanking3 += ranking[3];
+        List<RankingData> dataList = new List<RankingData>();
+        dataList.Add(a);
+        dataList.Add(b);
+        dataList.Add(c);
+        string json = JsonUtility.ToJson(dataList);
+        Debug.Log(json);
+        json = JsonHelper.ToJson<RankingData>(dataList.ToArray());
+        Debug.Log(json);
+        RankingData[] newList = JsonHelper.FromJson<RankingData>(json);
 
 
+        //今までの順位を持ってくる
+        string readText = "";
+        StreamReader reader;
+        reader = new StreamReader(Application.dataPath + "/save" + ranking + ".json");
+        readText = reader.ReadToEnd();
+        reader.Close();
+
+        m_rankingDate = JsonUtility.FromJson<RankingDate>(readText);
+
+
+
+        // オブジェクトからTextコンポーネントを取得
+        TextMeshProUGUI score_text = score_object.GetComponent<TextMeshProUGUI>();
+        score_text.text = "1,"  + "\n" + "2," +  "\n" + "3," ;
+        //ランキングを保存
+        StreamWriter writer;
+        string jsonstr = JsonUtility.ToJson(m_rankingDate);
+        writer = new StreamWriter(Application.dataPath + "/save" +ranking + ".json", false);
     }
-    void Swap(int x,  int y)
-    {
-        int tmp;
-        tmp = x;
-        x = y;
-        y = tmp;
-    }
+ 
+
     // Update is called once per frame
     void Update()
     {
+        
     }
 }
